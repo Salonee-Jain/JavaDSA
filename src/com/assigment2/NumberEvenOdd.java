bb9993cf09409130f134ba4497b58e8e218dd071
Salonee-Jain <salonijain1248@gmail.com>
2021-08-23 17:50:15 +0530

    added even odd file

--- src/com/assigment2/NumberEvenOdd.java
+++ src/com/assigment2/NumberEvenOdd.java
@@ -1,7 +1,19 @@
 package com.assigment2;
 
+import java.util.Scanner;
+
 public class NumberEvenOdd {
     public static void main(String[] args) {
+        System.out.println("NUmber: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a%2==0){
+            System.out.println("even");
+        }
+
+        else{
+            System.out.println("odd");
+        }
 
     }
 }
