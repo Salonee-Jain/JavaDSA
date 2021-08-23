3a760e15f9f7f6ed9bcba86466a4952a3613198f
Salonee-Jain <salonijain1248@gmail.com>
2021-08-23 18:18:43 +0530

    Added greater file

--- src/com/assigment2/Largest.java
+++ src/com/assigment2/Largest.java
@@ -1,4 +1,20 @@
 package com.assigment2;
 
+import java.util.Scanner;
+
 public class Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter no: ");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        if (a>b){
+            System.out.println(a+" is greater");
+        }
+        else{
+            System.out.println(b+" is greater");
+        }
+
+
+    }
 }
