b0fb7e744789022f08d3f2df7fbb95aeb6cb35a8
Salonee-Jain <salonijain1248@gmail.com>
2021-08-23 18:32:09 +0530

    Added Repeatnum file

--- src/com/assigment2/Repeatnum.java
+++ src/com/assigment2/Repeatnum.java
@@ -1,2 +1,19 @@
-package com.assigment2;public class Repeatnum {
+package com.assigment2;
+
+import java.util.Scanner;
+
+public class Repeatnum {
+    public static void main(String[] args) {
+        int a=1;
+        int sum=0;
+        while (a!=0){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter no: ");
+            a = sc.nextInt();
+            sum+=a;
+
+        }
+        System.out.println("sum is: "+sum);
+
+    }
 }
--- src/com/assigment2/Rupee.java
+++ src/com/assigment2/Rupee.java
@@ -1,4 +1,13 @@
 package com.assigment2;
 
+import java.util.Scanner;
+
 public class Rupee {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter no: ");
+        int a= sc.nextInt();
+        System.out.println(a+65);
+    }
+
 }
