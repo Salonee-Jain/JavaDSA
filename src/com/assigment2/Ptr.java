1bf4584779129a23f48cbb1e4560eed8ed4bea5c
Salonee-Jain <salonijain1248@gmail.com>
2021-08-23 17:58:06 +0530

    Added ptr file

--- src/com/assigment2/Ptr.java
+++ src/com/assigment2/Ptr.java
@@ -1,4 +1,17 @@
 package com.assigment2;
 
+import java.util.Scanner;
+
 public class Ptr {
+    public static void main(String[] args) {
+        System.out.println("Enter a principal: ");
+        Scanner sc = new Scanner(System.in);
+        float p = sc.nextFloat();
+        System.out.println("Enter a rate ");
+        float t = sc.nextFloat();
+        System.out.println("Enter a time: ");
+        float r = sc.nextFloat();
+        System.out.println("Simple interest: "+(p*t*r)/100);
+    }
+
 }
